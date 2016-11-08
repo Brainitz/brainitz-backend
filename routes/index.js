2ec1b6a8440f5716ac861e1b9cb1635c15484ad1
Jonathan Huxhold <jhuxhold@gmail.com>
2016-11-08 11:08:51 -0700

    adding aws

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
+var Brainitz = require('../lib/queries');
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  Brainitz.getEverything().then(function(everything) {
+    console.log("yo buddy");
+    res.json(everything);
+  })
 });
 
 module.exports = router;
