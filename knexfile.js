4385efb05f4c4ba3fce58fff65377b58a6fd4f60
Jonathan Huxhold <jhuxhold@gmail.com>
2016-11-13 14:05:27 -0700

    adding db connection

--- knexfile.js
+++ knexfile.js
@@ -4,7 +4,7 @@ module.exports = {
 
   development: {
     client: 'pg',
-    connection: process.env.DATABASE_URL
+    connection: process.env.DATABASE_URL + '?ssl=true'
   },
 
   production: {
--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,25 @@
 var express = require('express');
 var router = express.Router();
 var Brainitz = require('../lib/queries');
+var pgp = require('pg-promise')(/*options*/)
+var db = 
 
 
 /* GET home page. */
+
+
 router.get('/', function(req, res, next) {
-  Brainitz.getEverything().then(function(everything) {
-    console.log("yo buddy");
-    console.log(everything);
-  })
+  db.one('SELECT $1 AS value', 123)
+    .then(function (data) {
+      console.log('DATA:', data.value)
+    })
+    .catch(function (error) {
+      console.log('ERROR:', error)
+    })
+  // Brainitz.getEverything().then(function(everything) {
+  //   console.log("yo buddy");
+  //   console.log(everything);
+  // })
 });
 
 module.exports = router;
