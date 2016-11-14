3d4538808d8a234d46a91fe097605d2b84b8c878
Jonathan Huxhold <jhuxhold@gmail.com>
2016-11-13 18:55:35 -0700

    adding initial test query

--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,26 @@
+var promise = require('bluebird');
+
 var express = require('express');
 var router = express.Router();
 var Brainitz = require('../lib/queries');
-var pgp = require('pg-promise')(/*options*/)
-var db = 
+var knex = require('../db/knex');
+
+
+var options = {
+  promiseLib: promise
+};
+var pgp = require('pg-promise')(options);
+var db = pgp(process.env.DATABASE_URL);
 
 
 /* GET home page. */
 
-
 router.get('/', function(req, res, next) {
-  db.one('SELECT $1 AS value', 123)
-    .then(function (data) {
-      console.log('DATA:', data.value)
+  // db.one('SELECT $1 AS value', 123)
+  db.any(`select * from INFORMATION_SCHEMA.COLUMNS`)
+    .then(function(data) {
+      res.json(data);
+      console.log(data);
     })
     .catch(function (error) {
       console.log('ERROR:', error)
